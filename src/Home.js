c1cf76fa7164179192a1ac61b5b92e4cf9e0f502
echchabli_hamza <echchablihamza1@gmail.com>
2023-11-18 23:16:32 +0100

    props

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,19 @@
 import { useState } from "react";
-
-
+import BlogList from "./BlogList";
 const Home = () => {
-
-    
-    const [name , setName] = useState('hi');
-
-    const chanveValue = () =>{
-           setName('fuck off');
-    }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
     return (
 
       <div className="Home">
-      <h2>{name}</h2>
-      
-      <button onClick={chanveValue}>click me</button>
-      
+        <BlogList blogs={blogs} title="All Blogs" />
       </div>
-      
       
         
       );
 }
  
 export default Home;
-
