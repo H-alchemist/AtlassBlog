04ecfade6845c41eb0da1750805d62113d9c3062
echchabli_hamza <echchablihamza1@gmail.com>
2023-11-19 22:39:34 +0100

    loading messge fetch data && conddition

--- src/Home.js
+++ src/Home.js
@@ -2,34 +2,25 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ui', id: 3 },
-    { title: 'My new website', body: 'lorem ipsum...', author: 'ui', id: 4 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 5 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 6 }
-  ])
-
-  const [name, setName] = useState('mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null)
+  const [isLoading , setIsLoading]  = useState(true)
 
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-  }, [name])
+  setTimeout(() =>  {fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+
+        setBlogs(data)
+        setIsLoading(false)
+      })},1000);
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      {/* <button onClick={() => setName('luigi')}>change name</button> */}
-      <button onClick={() =>{setName("hamza")}} >change name</button><br></br>
-       <button onClick={() =>{setName("ahmed")}} >change name</button><br></br>
-       <p>{name}</p>
+      {isLoading && <div>Loading .....</div> }
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
