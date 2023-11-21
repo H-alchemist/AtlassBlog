1537272680580af51e15582d4b73d8c176e794f5
echchabli_hamza <echchablihamza1@gmail.com>
2023-11-21 18:49:01 +0100

    abort fetch process when it is  not neede

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import Test from './Test';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+
+            </Route>
+            <Route path="/Test">
+              <Test />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
+
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,12 @@
+
+
+const Test = () => {
+  return ( 
+    <div className="test">
+      <h1>Hello, this is a Test component!</h1>
+      <p>This is some content inside the component.</p>
+    </div>
+  );
+}
+
+export default Test ;
